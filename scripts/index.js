86a80806b82bc512e66c2f21c8c7d78532790a77
Florian Besnard <besnard.florian@outlook.com>
2020-09-25 18:36:02 +0200

    Added : Projects Debunker, WIP, DRush, Umbra

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,34 @@
 $(document).ready(function () {
     // INIT PAGE
-    $('body').fadeIn(1000).removeClass('hidden');
+    $('body').fadeIn(1000);
 
+    // HOME PAGE
     $('button:contains("HOME")').click(function (e) {
         e.preventDefault();
-        $(".centered_image").fadeIn();
-        $(".menu_text_position").fadeIn();
+        $("#projects").fadeOut(500).promise().done(function(){
+            $(".centered_image").fadeIn(500);
+            $(".menu_text_position").fadeIn(500);
+        });
     });
+
+    // PROJECTS PAGE
     $('button:contains("PROJECTS")').click(function (e) {
         e.preventDefault();
-        $(".centered_image").fadeOut();
-        $(".menu_text_position").fadeOut();
+        $(".centered_image, .menu_text_position").fadeOut(500).promise().done(function(){
+            $(".project_container").fadeIn(500);
+        });
     });
+    
+    // GD PAGE
     $('button:contains("GAME DESIGN")').click(function (e) {
         e.preventDefault();
-        $(".centered_image").fadeOut();
-        $(".menu_text_position").fadeOut();
+        $(".centered_image, .menu_text_position, .project_container").fadeOut(500);
     });
+    
+    // CONTACT PAGE
     $('button:contains("CONTACT")').click(function (e) {
         e.preventDefault();
-        $(".centered_image").fadeOut();
-        $(".menu_text_position").fadeOut();
+        $(".centered_image, .menu_text_position, .project_container").fadeOut(500);
+
     });
 });
